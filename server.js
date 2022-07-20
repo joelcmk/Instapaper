037830625215d1aff0fbc4424db100dbb6cdf21b
joelcmk <57118300+joelcmk@users.noreply.github.com>
2022-07-19 19:42:07 -0500

    installeed mongoose, express and cors

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const axios = require('axios');
-const express = require('express'); //Line 1
-const app = express(); //Line 2
+const express = require('express');
 const port = process.env.PORT || 5000; //Line 3
 const { convert } = require('html-to-text');
-var fs = require('fs');
 const puppeteer = require('puppeteer');
+
+const app = express();
+const cors = requre('cors');
+app.use(express.json());
+app.use(cors());
+import postRoutes from './routes/posts.js';
+import './db/db.js';
+app.use('/posts', postRoutes);
 
 // This displays message that the server running and listening to specified port
 app.listen(port, () => console.log(`Listening on port ${port}`)); //Line 6
@@ -25,7 +31,6 @@ const test = 'https://fs.blog/learning/';
   const page = await browser.newPage();
   await page.goto('https://fs.blog/learning/');
   const response = await page.goto(url);
-  //console.log(await response.text());
   const test = await response.text();
   dataTest.push({ test });
   await browser.close();
